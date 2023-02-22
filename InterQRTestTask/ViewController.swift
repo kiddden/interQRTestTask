eddfe5900d5b8e6f411aaaa4ee53f0b9aac93591
kiddden <kazzer@knu.ua>
2023-02-22 15:55:31 +0200

    Initial Commit

---
+++ InterQRTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterQRTestTask
+//
+//  Created by Eugene Ned on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
